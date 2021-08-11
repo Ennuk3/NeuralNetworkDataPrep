9ca1b0f24432724a2cf17f617df43f6638ca0413
Enn Veikesaar <ennveik@hotmail.com>
2021-08-11 02:10:10 +0100

    Index List
    Creates an index list based on the labels of the neural network model

---
+++ Functions/index_list_func.py
@@ -0,0 +1,11 @@
+def index_list_func(folder_path):
+    file_path = folder_path.replace("c\\", "")
+    file_name = "labels.txt"  
+    full_path = os.path.join(file_path, file_name)
+    index_file = pd.read_csv(full_path, delimiter = "\n", names = ["Labels"])
+    index_file = index_file["Labels"].to_list()
+    index_list = []
+    for index in index_file:
+        index = re.sub('\(.*?\)','',index)
+        index_list.append(index)
+    return index_list
