fdd90ed7fe620f80e58794b4cee2f915d4cc46e7
Enn Veikesaar <ennveik@hotmail.com>
2021-08-31 19:59:51 +0100

    Added save path

--- Examples/Read and Plot.py
+++ Examples/Read and Plot.py
@@ -2,10 +2,12 @@ from Functions import model_input_func
 from Functions import read
 from Functions import plot_pred_func
 
+savepath = r".\Plots\Model comparisons\\"
+
 filename = str(input("Filename to search for? (Must be identical for all searches) "))
 
 full_data, pred_list = read(folder_paths, folder_names, filename)[0], read(folder_paths, folder_names, filename)[1]
 
 full_data.to_csv(r".\Intermediate databases\Model Compilation Table.csv", line_terminator="\n", sep = " ", na_rep = " ")
 
-plot_pred_func(read(folder_paths, folder_names, filename)[0], read(folder_paths, folder_names, filename)[1])
+plot_pred_func(read(folder_paths, folder_names, filename)[0], read(folder_paths, folder_names, filename)[1], savepath)
