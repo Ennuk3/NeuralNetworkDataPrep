dbf01bf4411ead72168254dce74651add6c4acf4
Enn Veikesaar <ennveik@hotmail.com>
2021-08-31 20:01:36 +0100

    Corrected local paths

--- Examples/Predictor.py
+++ Examples/Predictor.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from Functions import pred_comp_func
 
-src_file_real = pd.read_csv(r"C:\Users\Enn\Documents\CDT projekt\Progre\Comps\Orikas.csv", header = None)
+src_file_real = pd.read_csv(r".\Example Databases\Composition files\Originals\Orikas.csv", header = None)
 
-ref_file_real = pd.read_csv(r"C:\Users\Enn\Documents\CDT projekt\Progre\Predictor_reference.csv", sep = ";", header = None)
+ref_file_real = pd.read_csv(r".\Example Databases\Composition files\References\Predictor_reference.csv", sep = ";", header = None)
 
 pred_comp_func(src_file_real, ref_file_real)
