04a8a0ba700fac4e5c54c5167f4563feb56013ef
Enn Veikesaar <ennveik@hotmail.com>
2021-09-01 19:35:48 +0100

    Added comments
    Added markdown and general comments for future documentation.

--- Functions/upper_shelf_func.py
+++ Functions/upper_shelf_func.py
@@ -8,6 +8,26 @@ from Functions import check_surr
 from Functions import gradient
 
 def upper_shelf_func(file, curve_plot_column, database):
+    """
+    This function reads in the raw CSV files and fits a Charpy curve (based on a sigmoid, tanh function) to the data. 
+    From there, it identifies the inflection point (or transition point in materials science) and the upper shelf start point.
+    The upper shelf start point is the first point in the tanh curve, where curve plateaus by a specified amount on the upper end. (see check_surr for more detail).
+    Then it adds all of the points (energy and temperature) into a single DataFrame and then saves it as CSV file for future processing.
+    
+    Parameters
+    ----------
+    file : DataFrame
+        raw CSV data
+    curve_plot_column : list
+        list of column names for the curve plots
+    database : DataFrame
+        empty DataFrame for the function to fill up with all of the appropriate points
+        
+    Returns
+    -------
+    
+    """
+    
     i = -1
     while i <= len(file):
         try:
@@ -19,7 +39,8 @@ def upper_shelf_func(file, curve_plot_column, database):
             comp = comp.to_numpy()
   
             pop1 = [max(row), np.median(curve_plot_column), 1, min(row)]
-       
+    
+            #Fit the curve according to the tanh_func.
             popt_tanh, pcov_tanh = curve_fit(tanh_func, curve_plot_column, row, pop1, maxfev = 2000)
             
             y_tanh = tanh_func(x, *popt_tanh)
@@ -27,19 +48,23 @@ def upper_shelf_func(file, curve_plot_column, database):
             data_tanh = plt.plot(x, y_tanh)
             plt.close()
             
+            #Calculate the first and second derivatives of the fitted tanh function.
             es_y_tanh = np.gradient(y_tanh)
             te_y_tanh = np.gradient(np.gradient(y_tanh))
             
+            #Isolate the data from the plot.
             data_source_tanh = data_tanh[0].get_data()
             x_data_source_tanh = data_source_tanh[0]
             y_data_source_tanh = data_source_tanh[1]
             transition_points_tanh = []
             upper_shelf_tanh = []
                                    
-            index_tanh = np.where((es_y_tanh == np.max(es_y_tanh)) & (te_y_tanh < 0.000001))[0][0]
+            #Inflection point calculation.
+            index_tanh = np.where((es_y_tanh == np.max(es_y_tanh)) & (te_y_tanh < 0.000001))[0][0] 
             transition_points_tanh.append(x_data_source_tanh[index_tanh])
             transition_points_tanh.append(y_data_source_tanh[index_tanh])          
                               
+            #Initial upper shelf start point approximation, see check_surr and gradient for more details.
             index2_tanh = np.where((es_y_tanh < 0.1) & (te_y_tanh < 0.0001) & (y_tanh >= np.max(row)-30) | (y_tanh >= np.max(row)))[0][0]
             index2_tanh = check_surr(y_data_source_tanh, index2_tanh)
             
@@ -51,6 +76,7 @@ def upper_shelf_func(file, curve_plot_column, database):
         except IndexError:
             continue
    
+        #Compile the data as a single row within the new DataFrame.
         upper_shelf_tanh.append(x_data_source_tanh[index2_tanh])
         upper_shelf_tanh.append(y_data_source_tanh[index2_tanh])
    
