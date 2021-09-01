0e3d8f9fe4b58ee0a4526df719b6a9456494a0ad
Enn Veikesaar <ennveik@hotmail.com>
2021-09-01 19:19:48 +0100

    Added comments
    Added markdown and general comments for future documentation.

--- Functions/rename_func.py
+++ Functions/rename_func.py
@@ -1,6 +1,22 @@
 import os
 
 def rename_func(dirpath, filename):
+    """
+    This function takes an extensionless CSV file and adds the ".csv" extension to it.
+    This is necessary to read and verify the files in a Windows environment.
+    
+    Parameters
+    ----------
+    dirpath : str
+        path of the file
+    filename : str
+        name of the file
+    
+    Returns
+    -------
+    No direct returns from this function, however it changes the filename within the file system, allowing for users to read the file within a Windows environment.
+    """
+    
     file_old = os.path.join(dirpath, filename)
     file_new = os.path.join(dirpath, filename + ".csv")
     os.rename(file_old, file_new)
