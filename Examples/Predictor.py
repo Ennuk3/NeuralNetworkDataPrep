82256806a0e4d1b5ba1b4a2e7f3c42c3f10a2bce
Enn Veikesaar <ennveik@hotmail.com>
2021-09-01 19:42:07 +0100

    Added comments
    Added markdown and general comments for future documentation.

--- Examples/Predictor.py
+++ Examples/Predictor.py
@@ -1,6 +1,13 @@
 import pandas as pd
 from Functions import pred_comp_func
 
+"""
+This example:
+1. reads in a source file (for the structure of the file),
+2. reads in a reference file (containing all of the compositional profiles to be created,
+3. saves every compositional profile as a separate file in a designated folder for future predictions.
+"""
+
 src_file_real = pd.read_csv(r".\Example Databases\Composition files\Originals\Orikas.csv", header = None)
 
 ref_file_real = pd.read_csv(r".\Example Databases\Composition files\References\Predictor_reference.csv", sep = ";", header = None)
