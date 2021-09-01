09436661b851e7fdefa60463e119db187da7fbc2
Enn Veikesaar <ennveik@hotmail.com>
2021-09-01 19:43:51 +0100

    Added comments
    Added markdown and general comments for future documentation.

--- Examples/Significances.py
+++ Examples/Significances.py
@@ -2,7 +2,14 @@ from Functions import model_input_func
 from Functions import sig_data_func
 from Functions import plot_sig_func
 
-path_real = r".\Example significance files\"
+"""
+This example:
+1. creates a list of models,
+2. creates a multi-index DataFrame for the significance values of each submodel within every model committee,
+3. plots the averages of every submodel.
+"""
+
+path_real = r".\Example significance files\\"
 
 folder_list_real, folder_path_list_real = model_input_func(path_real)
 
