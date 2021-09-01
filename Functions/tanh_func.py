cb1f0e181aa44e3e4b3478a52eb240778985c61f
Enn Veikesaar <ennveik@hotmail.com>
2021-09-01 19:29:14 +0100

    Added comments
    Added markdown and general comments for future documentation.

--- Functions/tanh_func.py
+++ Functions/tanh_func.py
@@ -1,5 +1,22 @@
 import numpy as np
 
 def tanh_func(x, n, x_0, m, c):
+    """
+    This function defines tanh in order to use it to fit Charpy curves.
+    
+    Parameters
+    ----------
+    x : float
+        independent variable
+    n : float
+    x_0 : float
+    m : float
+    c : float
+    
+    Returns
+    -------
+    y : float
+        dependent variable
+    """
     y = m*np.tanh((x-x_0)/n)+c
     return y
