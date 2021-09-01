b3d263e215385309208263745b6966557d2510eb
Enn Veikesaar <ennveik@hotmail.com>
2021-09-01 19:10:29 +0100

    Added comments
    Added markdown and general comments for future documentation.

--- Functions/pred_comp_func.py
+++ Functions/pred_comp_func.py
@@ -1,6 +1,25 @@
 import pandas as pd
 
 def pred_comp_func(src_file, ref_file, path):
+    """
+    This function creates compositional profile files for the neural network predictor program in an automated fashion.
+    
+    Parameters
+    ----------
+    src_file : DataFrame
+        DataFrame of the original profile file, necessary for the original structure
+    ref_file : DataFrame
+        DataFrame of the desired compositional profiles
+    path : str
+        save directory
+        
+    Returns
+    -------
+    Nothing directly from the function, however it creates as many compositional profiles as are written within the reference file. 
+    This function is written for a very specific compositional profile. 
+    For more general use, the index values for both the source and the reference file need to be changed appropriately.
+    """
+    
     i = 0
     
     while i < len(ref_file):
