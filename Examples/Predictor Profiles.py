061d1f205ca59e7754f5c97a8fa1dfdfa0ba751e
Enn Veikesaar <ennveik@hotmail.com>
2021-09-03 00:58:50 +0100

    Added bracket + some text

--- Examples/Predictor Profiles.py
+++ Examples/Predictor Profiles.py
@@ -4,8 +4,8 @@ from Functions import pred_comp_func
 """
 This example:
 1. reads in a source file (for the structure of the file),
-2. reads in a reference file (containing all of the compositional profiles to be created,
-3. saves every compositional profile as a separate file in a designated folder for future predictions.
+2. reads in a reference file (containing all of the compositional profiles to be created),
+3. saves every compositional profile as a separate file in a designated folder to be used for future predictions.
 """
 
 src_file_real = pd.read_csv(r".\Example Databases\Composition files\Originals\Orikas.csv", header = None)
