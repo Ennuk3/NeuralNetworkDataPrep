b7d5df740a1f3777127e0e09369de3c3be2dc902
Enn Veikesaar <ennveik@hotmail.com>
2021-09-03 19:15:39 +0100

    Test

--- Tests/Transition + USSP_test.py
+++ Tests/Transition + USSP_test.py
@@ -1,3 +1,58 @@
-i = 1+1
-def test_func():
-  assert i == 3, "Oh no!"
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.optimize import curve_fit
+import pandas as pd
+import warnings
+
+from Functions import tanh_func
+from Functions import check_surr
+from Functions import gradient
+from Functions import upper_shelf_func
+
+"""
+This example:
+1. reads in a raw CSV file, 
+2. fits a tanh curve to the raw data, 
+3. calculates the inflection and upper shelf start point for each fitted tanh curve (see upper_shelf_func for more details),
+4. returns these values as a CSV file for future use.
+"""
+
+warnings.filterwarnings("ignore", category=RuntimeWarning) 
+
+dirpath = r".\Example Databases\Full Charpy Curves\Full_database_MicroAlloying.csv"
+
+file_real = pd.read_csv(dirpath, delimiter = ";")
+
+def test_empty_raw_data():
+  i = -1
+  np.where(pd.isnull(file_real), i = 1, i = 0)
+  assert i == 1, "The raw data contains empty cells. Please remove all empty cells, otherwise NeuroMat will not give valid results."
+  
+
+column = file_real.columns.tolist()
+
+curve_column = column[17:34] #Change these columns according to the source csv
+
+curve_plot_column_real = []
+
+for element in curve_column:
+    curve_plot_column_real.append(int(element))
+    
+x = np.linspace(-120, 40,10000)
+
+columns_comp = column[0:16]
+
+columns_result = ["TransitionT", "TransitionJ", "UpperT", "UpperJ"]
+
+columns_list = np.concatenate([columns_comp,columns_result])
+
+database_real = pd.DataFrame(columns = columns_list)
+    
+final_database = upper_shelf_func(file_real, curve_plot_column_real, database_real)
+
+final_database.to_csv(r".\Intermediate databases\UpperTJ+TranTJ_Micro.csv", index = False)
+
+def test_empty_final_data():
+  i = -1
+  np.where(pd.isnull(final_database), i = 1, i = 0)
+  assert i == 1, "The processed data contains empty cells. Please remove all empty cells, otherwise NeuroMat will not give valid results."
